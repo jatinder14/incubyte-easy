b89aa1b23be2dd18c4e64bd5bda819041f2c0154
jatinder14 <jatinder1901243@gmail.com>
2025-03-27 18:25:40 +0530

    added the test cases

---
+++ test/calculator.test.js
@@ -0,0 +1,58 @@
+const add = require('../calculator');
+
+
+describe('String Calculator', () => {
+
+    it('should return 0 for an empty string', () => {
+        let result = add("");
+        expect(result).toBe(0);
+    });
+
+    it('should return number itself for single number', () => {
+        let result = add("5");
+        expect(result).toBe(5);
+    });
+
+    it('should return sum of numbers seperated by commas', () => {
+        let result = add("5,1");
+        expect(result).toBe(6);
+    });
+
+    it('should return sum of numbers seperated by commas or new line', () => {
+        let result = add("5,\n2");
+        expect(result).toBe(7);
+    });
+
+    it('should return sum of numbers seperated by commas or new line', () => {
+        let result = add("1\n2,3");
+        expect(result).toBe(6);
+    });
+
+    it('should handle delimeters', () => {
+        let result = add("//;\n1;;2;;5");
+        expect(result).toBe(8);
+    });
+
+    it('should handle delimeters', () => {
+        let result = add("//;\n1;2");
+        expect(result).toBe(3);
+    });
+
+    it('should handle delimiters of any length', () => {
+        let result = add("//[***]\n1***2***3");
+        expect(result).toBe(6);
+    });
+
+    it('should handle delimiters with multiple characters and mixed delimiters', () => {
+        let result = add("//[*][%]\n1*2%3");
+        expect(result).toBe(6);
+    });
+
+    it("should throw an error when a negative number is encountered", function () {
+        expect(() => add("-1,2,-4")).toThrowError("Negative numbers not allowed: -1, -4");
+    });
+    
+    it("should throw an error when a negative number is encountered", function () {
+        expect(() => add("//;\n1;-2")).toThrowError("Negative numbers not allowed: -2");
+    });
+});
